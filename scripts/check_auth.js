2ad28e2abc2b67b4fbef7af5d34c9207ba618130
Ilya Dyb <ilya77788899@mail.ru>
2024-07-10 01:17:23 +0300

    .

--- scripts/check_auth.js
+++ scripts/check_auth.js
@@ -1,4 +1,4 @@
-if (localStorage.getItem('access') === null || localStorage.getItem('access') === NaN) {
-    var authPages = document.getElementById("authPages")
-    authPages.className = "hidden"
-}
+// if (localStorage.getItem('access') === null || localStorage.getItem('access') === NaN) {
+//     var authPages = document.getElementById("authPages")
+//     authPages.className = "hidden"
+// }
