fed6ad8f9599f667497a0670571b67e43d462ac6
Ilya Dyb <ilya77788899@mail.ru>
2024-07-17 20:29:17 +0300

    update login problem detail notif

--- scripts/auth.js
+++ scripts/auth.js
@@ -4,26 +4,6 @@
 //     .catch(err => console.error(err));
 
 
-async function login(usernameOrEmail, password) {
-    const response = await fetch('http://127.0.0.1:8000/api/user/login/', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            username_or_email: usernameOrEmail,
-            password: password
-        })
-    });
-
-    if (!response.ok) {
-        throw new Error('Login failed');
-    }
-
-    const data = await response.json();
-    localStorage.setItem('accessToken', data.access);
-    localStorage.setItem('refreshToken', data.refresh);
-}
 
 // Пример вызова функции login
 // login('user@example.com', 'password')
